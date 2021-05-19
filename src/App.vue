11ec16807055b1c27c24afd84d9a06a60fac5386
tangyuchen <644169917@qq.com>
2021-05-19 21:09:48 +0800

    测试脚手架

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+    <!-- router-link 定义点击后导航到哪个路径下 -->
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </header>
+    <!-- 对应的组件内容渲染到router-view中 -->
+    <router-view></router-view> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Hello from './components/Hello.vue'
+import About from './components/About.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Hello,
+    About
   }
 }
 </script>
@@ -24,5 +30,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+a{
+  margin:0 10px
 }
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Hello from "../components/Hello.vue";
+import About from '../components/About.vue'
+
+Vue.use(Router);
+
+export default new Router({
+	routes: [
+		{
+			path: "/",
+			component: Hello,
+		},
+		{
+			path: "/about",
+			component: About,
+		},
+	],
+});
